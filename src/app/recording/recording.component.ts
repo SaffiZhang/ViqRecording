924aa0172ced9ec85edaedea1b2aa6a3deda7969
Saffi Zhang <szhang@viqsolutions.com>
2019-07-10 17:48:59 -0400

    Add update API

--- src/app/recording/recording.component.ts
+++ src/app/recording/recording.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService} from'../API.service';
 import {UpdateViqRecordingInput} from'../API.service';
+import {GetViqRecordingQuery} from'../API.service';
 
 @Component({
   selector: 'app-recording',
@@ -8,27 +9,47 @@ import {UpdateViqRecordingInput} from'../API.service';
   styleUrls: ['./recording.component.scss']
 })
 export class RecordingComponent implements OnInit {
-  updateRecordingInput:UpdateViqRecordingInput;
-  recording={recordings:[{url:"",lastModified:""}], annotations:[]};
+  updateRecordingInput:any;
+  recording={Id:'0',annotations:[]};
   
   constructor(private api:APIService) { }
   
   async ngOnInit() {
-     this.recording=await this.getRecording('11111');
-     this.updateRecordingInput=Object.assign(this.recording);
-     this.updateRecordingInput.annotations.push(
-      {
-        "annotationTime": "2019-06-20 18:17:38",
-        "text": "add new annotation"
-      });
-    /*   await this.updateRecording(this.updateRecordingInput);  */
+     this.recording=await this.getRecording('2');
+     this.assignUpdateInput();
+     let result =await this.updateRecording(this.updateRecordingInput);
   
      
   }
    async updateRecording(recording){
-    let result= await this.api.UpdateViqRecording(recording)
+     try{
+      let result= await this.api.UpdateViqRecording(recording)
+     }catch(e){
+       console.log(e);
+     }
+    
   }
   async getRecording(id){
     return await this.api.GetViqRecording(id);
+  }
+  assignUpdateInput()
+  {
+    this.updateRecordingInput={
+      Id: '2',
+      interviewee:'Tom Cruise',
+      recordings: [
+        {
+          "lastModified": "Jul 4, 2019 2:53:16 PM GMT-0400",
+          "url": "https://viq-recording.s3.amazonaws.com/SR607E21/2019/06/28/SR607E21-20190628_181526-audio_P1-1.mp3"
+        }
+      ]
+    }
+  }
+  getAnnotation(annotationPromise)
+  {
+    var annotation={annotationTime:'',text:''};
+    annotation.annotationTime=annotationPromise.annotationTime;
+    annotation.text=annotationPromise.text;
+    return annotation;
   }
 }
