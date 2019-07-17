fcfc9f32d9794879c927752bcfb8a2cf3bf4278d
Yubin Zhu <yubin.zhu.68@gmail.com>
2019-07-17 19:14:26 -0400

    file upload with path

--- src/app/add-attachment/add-attachment.component.ts
+++ src/app/add-attachment/add-attachment.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FileUploadService} from '../services/file-upload.service';
 
 @Component({
@@ -12,6 +12,9 @@ export class AddAttachmentComponent implements OnInit {
   public saved: EventEmitter<any> = new EventEmitter<any>();
   @Output()
   public cancelled: EventEmitter<any> = new EventEmitter<any>();
+
+  @Input('target-folder')
+  public targetFolder: string;
 
   public description: string;
 
@@ -29,7 +32,7 @@ export class AddAttachmentComponent implements OnInit {
 
   public save() {
     const file = this.selectedFiles.item(0);
-    this.fileUploadService.uploadFile(file);
+    this.fileUploadService.uploadFile(file, this.targetFolder);
     this.saved.emit({});
   }
 
