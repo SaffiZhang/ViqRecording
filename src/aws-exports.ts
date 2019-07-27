2e0cb912e6f5163151b9ffdd5a0686b18b3d083e
Saffi Zhang <szhang@viqsolutions.com>
2019-07-26 20:15:01 -0400

    add mulitple env

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -2,18 +2,18 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:78294458-6da3-4438-8213-554d611218bf",
+    "aws_cognito_identity_pool_id": "us-east-1:ceffae04-07cb-4b07-a443-f0d2d2b052ed",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_xPhvgy3pM",
-    "aws_user_pools_web_client_id": "7jcj6k2a40m240rplplc7h9nko",
+    "aws_user_pools_id": "us-east-1_308Aytf7T",
+    "aws_user_pools_web_client_id": "3cih6joe79j4ana0h08l16sn9o",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://26tegmh4hvfp5dpdqgwz76klwe.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://b4ivejwhnnbo3l3u6ijzi4olka.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-ic7ya3hjijb65beiauxbliu2iu",
-    "aws_content_delivery_bucket": "viq-recording-portal-hosting-test",
+    "aws_appsync_apiKey": "da2-iingptzzujb5xibnemifcgrnvm",
+    "aws_content_delivery_bucket": "viq-recording-portal-hosting-dev",
     "aws_content_delivery_bucket_region": "us-east-1",
-    "aws_content_delivery_url": "http://viq-recording-portal-hosting-test.s3-website-us-east-1.amazonaws.com"
+    "aws_content_delivery_url": "http://viq-recording-portal-hosting-dev.s3-website-us-east-1.amazonaws.com"
 };
 
 
