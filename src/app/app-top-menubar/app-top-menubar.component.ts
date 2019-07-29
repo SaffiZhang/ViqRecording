91d8a5626981fe52b3759a957ed7ba5032818b53
Yubin Zhu <yubin.zhu.68@gmail.com>
2019-07-29 11:48:47 -0400

    auth hookup

--- src/app/app-top-menubar/app-top-menubar.component.ts
+++ src/app/app-top-menubar/app-top-menubar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Auth from '@aws-amplify/auth';
+import {Router} from '@angular/router';
+import {AmplifyService} from 'aws-amplify-angular';
+import {auth} from 'aws-amplify-angular/dist/src/assets/data-test-attributes';
 
 @Component({
   selector: 'app-app-top-menubar',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppTopMenubarComponent implements OnInit {
 
-  constructor() { }
+  public isAuthenticated = false;
+  public user: any;
 
-  ngOnInit() {
+  constructor(private router: Router,
+              private amplifyService: AmplifyService,) {
   }
 
+  ngOnInit() {
+    this.amplifyService.authStateChange$.subscribe(authState => {
+      this.isAuthenticated = authState.state === 'signedIn';
+      this.user = authState.user;
+    });
+  }
+
+  public signOut($event) {
+    $event.preventDefault();
+    Auth.signOut().then(r => {
+      console.log('sigout:', r);
+      this.router.navigate(['/auth']);
+    });
+  }
 }
