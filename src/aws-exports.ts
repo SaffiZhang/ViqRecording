2bae9fb12ee6be7d2624b32478da7092d11b67a1
Saffi Zhang <szhang@viqsolutions.com>
2019-08-09 18:19:23 -0400

    3 level links

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -10,7 +10,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://ejrv5vpdbraqrmozadxwb2ipo4.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-sm7gt64mxvfr3pagc7us36xsdq",
+    "aws_appsync_apiKey": "da2-ct23ygrw5jembory577b4eyxoe",
     "aws_content_delivery_bucket": "viq-recording-portal-hosting-staging",
     "aws_content_delivery_bucket_region": "us-east-1",
     "aws_content_delivery_url": "http://viq-recording-portal-hosting-staging.s3-website-us-east-1.amazonaws.com"
