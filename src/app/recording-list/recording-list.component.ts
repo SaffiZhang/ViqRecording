d432e063af01b72ced22803082124d66c944e489
Yubin Zhu <yubin.zhu.68@gmail.com>
2019-08-20 08:11:45 -0400

    adding rows per page dropdown to case list page

--- src/app/recording-list/recording-list.component.ts
+++ src/app/recording-list/recording-list.component.ts
@@ -24,6 +24,27 @@ export class RecordingListComponent implements OnInit {
 
   public searchModel = {};
 
+  public rows = 10;
+  public rowsOptions = [{
+    label: '5',
+    value: 5
+  }, {
+    label: '10',
+    value: 10
+  }, {
+    label: '15',
+    value: 15
+  }, {
+    label: '20',
+    value: 20
+  }, {
+    label: '25',
+    value: 25
+  }, {
+    label: '50',
+    value: 50
+  }]
+
   constructor(private api: APIService) {
   }
 
